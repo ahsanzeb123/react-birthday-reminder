a8ff5bac7b3c6b28c112a195ff1313cb1dbb1234
ahsanzeb123 <ahsanzebkhan123@gmail.com>
2023-03-30 02:31:36 +0500

    this is first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import dare from './data';
+import List from './List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+function App () {
+  const [people,setPeople] = useState(dare);
+  const showList = () =>{
+     setPeople(dare);
+     document.getElementById('button1').style.display = 'block'
+     document.getElementById('button2').style.display = 'none'
+      document.getElementById('count1').style.display = "none";
+     document.getElementById('count').style.display = "block";
+      
+  }
+  const clicking =()=>{
+    setPeople([]);
+  
+     document.getElementById('button1').style.display = 'none'
+     document.getElementById('button2').style.display = 'block'
+     document.getElementById('count1').style.display = "block";
+     document.getElementById('count').style.display = "none";
+
+    
+
+  }
+  return(
+    <main>
+      <section id='23' className="container">
+       <h3 id='count' >{people.length} birthdays today</h3>
+       <h3 id='count1' >Show birthdays </h3>
+       <List people={people} />
+       <button className='btn1' id='button1'
+         onClick={clicking}  >Clear all</button>
+         <button className='btn2' id='button2' onClick={showList} >Show All</button>
+        
+      </section>
+    </main>
   );
 }
 
